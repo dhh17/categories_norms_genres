dcab602b41186e539662fee2c1868f55c8210b9f
Peter Ivanics <peter@choicely.com>
2017-05-18 21:10:57 +0300

    Formatting

--- chart_plotter/number_of_poems_line_chart.R
+++ chart_plotter/number_of_poems_line_chart.R
@@ -12,11 +12,13 @@ draw_number_of_poems_by_year_chart = function(data) {
     number_of_poems = c(number_of_poems, number) # append the number of poems to number_of_poems
   }
   
-  plot(x = years, y = number_of_poems, type = "line", xlab = "Year", ylab = "Number of poems", col = "red") # create the line plot
+  plot(x = years, y = number_of_poems, type = "line", xlab = "Year", ylab = "Number of poems", col = "lightpink", cex.main = 2, main = "Number of poems over years", lwd = 4) # create the line plot
+  points(years, number_of_poems, col = "red", pch = 19)
   
   article_index = read.csv("data/poem_count_from_article_index.csv", sep = "\t")
   article_index = article_index[which(article_index$Year <= max(years)),] # reducing the data
-  lines(article_index$Year, article_index$Poems, col = "blue")
+  lines(article_index$Year, article_index$Poems, col = "lightblue", lwd = 4)
+  points(article_index$Year, article_index$Poems, col = "blue", pch = 19)
   
   differences = abs(article_index$Poems - number_of_poems) # calculate the differences in poems between the amount predicted by the algorithm and the count valeus from the article index
   cat("Year with the biggest difference:", years[which(differences == max(differences))]) 
