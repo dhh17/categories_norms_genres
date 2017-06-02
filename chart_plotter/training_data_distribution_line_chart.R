e7d600747e88014d24c4aac9ab01a1e3ae27ff67
Peter Ivanics <peter@choicely.com>
2017-06-02 14:26:02 +0300

    Added training data plotter

---
+++ chart_plotter/training_data_distribution_line_chart.R
@@ -0,0 +1,21 @@
+draw_number_of_poems_by_year_chart = function(data) {
+  from = min(data$Date) # select the first year for the chart
+  to = max(data$Date) # select the last year for the chart
+  
+  years = seq(from, to) # generate sequence of years between the value of from and to (1820-1870) 
+  number_of_poems = c()
+  
+  for (i in years) {
+    poems_in = data[which(data$Date == i),] # select all poems in the year i
+    number = nrow(poems_in) # count the poems
+    
+    number_of_poems = c(number_of_poems, number) # append the number of poems to number_of_poems
+  }
+  
+  plot(x = years, y = number_of_poems, type = "line", xlab = "Year", ylab = "Number of textblocks", col = "lightpink", cex.main = 2, main = "Number of textblocks in training data", lwd = 4) # create the line plot
+  points(years, number_of_poems, col = "red", pch = 19)
+}
+
+data = read.csv("../data/docs.csv", sep = "\t")
+data$Date = strtoi(substr(data$Date, 7, 10))
+draw_number_of_poems_by_year_chart(data)
