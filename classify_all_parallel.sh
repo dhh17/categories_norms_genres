45a2d1b790342fb46426e91b93d58e0ad6c74386
Mikko Koho <mikko.koho@aalto.fi>
2017-06-05 17:57:22 +0300

    Bash script to classify one decade at a time in parallel

---
+++ classify_all_parallel.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+date
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1820 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1821 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1822 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1823 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1824 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1825 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1826 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1827 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1828 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1829
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1830 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1831 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1832 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1833 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1834 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1835 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1836 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1837 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1838 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1839
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1840 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1841 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1842 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1843 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1844 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1845 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1846 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1847 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1848 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1849
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1850 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1851 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1852 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1853 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1854 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1855 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1856 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1857 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1858 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1859
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1860 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1861 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1862 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1863 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1864 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1865 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1866 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1867 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1868 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1869
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1870 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1871 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1872 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1873 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1874 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1875 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1876 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1877 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1878 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1879
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1880 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1881 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1882 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1883 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1884 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1885 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1886 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1887 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1888 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1889
+
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1890 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1891 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1892 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1893 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1894 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1895 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1896 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1897 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1898 &
+python3 classify.py ~/dhh17data/newspapers/newspapers/fin/1899
+date
