338816bd5486197ef2b2b6b16d478f9d862e13b4
Mikko Koho <mikko.koho@iki.fi>
2017-06-08 15:06:37 +0300

    Update gather_training_data.sh to skip training data without line breaks

--- gather_training_data.sh
+++ gather_training_data.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+rm poemblocks/*
+rm nonpoemblocks/*
 python poem_reader.py ~/dhh17data/newspapers/
-python split_to_files.py data/additional_poemtexts.txt
-ls -l data/additional_poemtexts_* | wc -l
-mv data/additional_poemtexts_* poemblocks/
+# python split_to_files.py data/additional_poemtexts.txt
+# ls -l data/additional_poemtexts_* | wc -l
+# mv data/additional_poemtexts_* poemblocks/
